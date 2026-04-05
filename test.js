fdca9cef5fc8363eaab8754b22145ed6c1ced5c2
plaguery <sophiayan111@gmail.com>
2026-04-05 15:19:25 -0400

    completed roblox badge id methods

--- test.js
+++ test.js
@@ -11,20 +11,26 @@ async function fetchId(id) {
 
   const data = await response.json();
   const bloxId = data.robloxID;
-  console.log(bloxId);
   return bloxId;
 }
 
 //checks if roblox user uid owns item itemId
 //returns true if owned
-async function ownsBadge(uid, itemId) {
+async function ownsBadge(uid, badgeId) {
   const response = await fetch(
-    `https://inventory.roblox.com/v1/users/${uid}/items/2/${itemId}/is-owned`,
+    `https://inventory.roblox.com/v1/users/${uid}/items/2/${badgeId}/is-owned`,
   );
   const isOwned = await response.json();
   // console.log(isOwned);
   return isOwned;
 }
 
-fetchId("694605691716894820");
+async function checkUser(id, badgeId) {
+  const bloxId = await fetchId(id);
+  const isOwned = await ownsBadge(bloxId, badgeId);
+  console.log(isOwned);
+  return isOwned;
+}
+
 //console.log(ownsBadge(fetchId("694605691716894820"), "1078926980379768"));
+checkUser("694605691716894820", "1078926980379768");
