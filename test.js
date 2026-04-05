2646f2ac1bae4f332f675e7d6deda00c3a4bad76
plaguery <sophiayan111@gmail.com>
2026-04-05 15:58:25 -0400

    error checking

--- test.js
+++ test.js
@@ -2,35 +2,58 @@ const { clientId, guildId, token, blKey } = require("./config.json");
 
 //grabs roblox id from discord id using bloxlink
 async function fetchId(id) {
-  const response = await fetch(
-    `https://api.blox.link/v4/public/guilds/${guildId}/discord-to-roblox/${id}`,
-    {
-      headers: { Authorization: blKey },
-    },
-  );
+  try {
+    const response = await fetch(
+      `https://api.blox.link/v4/public/guilds/${guildId}/discord-to-roblox/${id}`,
+      {
+        headers: { Authorization: blKey },
+      },
+    );
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error(`bloxlink error: ${response.status} // ${data.error}`);
+    }
 
-  const data = await response.json();
-  const bloxId = data.robloxID;
-  return bloxId;
+    const bloxId = data.robloxID;
+    return bloxId;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 //checks if roblox user uid owns item itemId
 //returns true if owned
-async function ownsBadge(uid, badgeId) {
-  const response = await fetch(
-    `https://inventory.roblox.com/v1/users/${uid}/items/2/${badgeId}/is-owned`,
-  );
-  const isOwned = await response.json();
-  // console.log(isOwned);
-  return isOwned;
+async function ownsItem(uid, itemId, itemType = 2) {
+  //false if no valid uid
+  if (!uid) {
+    return false;
+  }
+  try {
+    const response = await fetch(
+      `https://inventory.roblox.com/v1/users/${uid}/items/${itemType}/${itemId}/is-owned`,
+    );
+    const isOwned = await response.json();
+
+    if (!response.ok) {
+      throw new Error(
+        `roblox error: ${response.status} // ${isOwned.errors[0].message}`,
+      );
+    }
+
+    // console.log(isOwned);
+    return isOwned;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-async function checkUser(id, badgeId) {
+//check from discord id & itemId
+async function checkUser(id, itemId, itemType = 2) {
   const bloxId = await fetchId(id);
-  const isOwned = await ownsBadge(bloxId, badgeId);
+  const isOwned = await ownsItem(bloxId, itemId, itemType);
   console.log(isOwned);
   return isOwned;
 }
 
-//console.log(ownsBadge(fetchId("694605691716894820"), "1078926980379768"));
-checkUser("694605691716894820", "1078926980379768");
+//checkUser("694605691716894820", "1078926980379768");
+ownsItem("29694111114867", "1078926980379768");
